67042e062ae777fb141f0d865d44d192582a0e66
Serena Ziviani <serena.ziviani@cern.ch>
2018-07-09 16:45:09 +0200

    poc: delete unused MPI error checking
    The MPI framework in its default settings handles automatically
    errors that occurs in MPI call by aborting the MPI job. As a
    conseguence, the function check_error is never called in the presence
    of an error.
    
    Delete the function and the ret variable to clean the code.

--- poc.cpp
+++ poc.cpp
@@ -15,14 +15,6 @@ const int WORKTAG = 1024;
 
 int MPI_MASTER_THREAD;
 
-void check_error(int ret, std::string err_string)
-{
-    if (ret != MPI_SUCCESS) {
-        std::cout << err_string << ": " << ret << std::endl;
-        exit(1);
-    }
-}
-
 class Scheduler
 {
     private:
@@ -69,14 +61,12 @@ struct Task {
                 m.unlock();
 
                 std::cout << std::string(2*(id_ + 1), '\t') << "MST " << id_ << ": Sending " << name_ << " to node " << gpu_pe << std::endl;
-                int ret = MPI_Send((void *)name_.c_str(), name_.size() + 1, MPI_CHAR, gpu_pe, WORKTAG + id_, MPI_COMM_WORLD);
+                MPI_Send((void *)name_.c_str(), name_.size() + 1, MPI_CHAR, gpu_pe, WORKTAG + id_, MPI_COMM_WORLD);
                 std::cout << std::string(2*(id_ + 1), '\t') << "MST " << id_ << ": Sent " << name_ << " to node " << gpu_pe << " tag " << WORKTAG + id_ << std::endl;
-                check_error(ret, "Error in MPI_Send, master thread");
 
                 MPI_Status status;
-                ret = MPI_Recv(0, 0, MPI_INT, gpu_pe, WORKTAG + id_, MPI_COMM_WORLD, &status);
+                MPI_Recv(0, 0, MPI_INT, gpu_pe, WORKTAG + id_, MPI_COMM_WORLD, &status);
                 std::cout << std::string(2*(id_ + 1), '\t') << "MST " << id_ << ": Received answer from " << gpu_pe << " tag " << WORKTAG + id_ << std::endl;
-                check_error(ret, "Error in MPI_Recv, master thread");
 
                 /* do other work */
                 return true;
@@ -100,29 +90,23 @@ struct Receiver {
         }
         void operator()() {
 
-            int count, ret;
+            int count;
             MPI_Status status;
-            MPI_Request request;
-            int abs_id = mpi_id_ * pool_size_ + id_;
 
             while (id_ == 0) {
                 std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " probing for work " << std::endl;
-                ret = MPI_Probe(MPI_MASTER_THREAD, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-                check_error(ret, "Error in MPI_Probe, abs_id " + abs_id);
-                std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " probed for tag " << status.MPI_TAG << ", node " << status.MPI_SOURCE << std::endl;
+                MPI_Probe(MPI_MASTER_THREAD, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " probed tag " << status.MPI_TAG << ", node " << status.MPI_SOURCE << std::endl;
 
                 if (status.MPI_TAG == DIETAG) {
                     std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " exiting " << std::endl;
                     return;
                 }
 
-                ret = MPI_Get_count(&status, MPI_CHAR, &count);
-                check_error(ret, "Error in MPI_Get_count, abs_id" + abs_id);
+                MPI_Get_count(&status, MPI_CHAR, &count);
 
                 char *rec_buf = new char[count];
-                ret = MPI_Recv(static_cast<void *>(rec_buf), count, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
-                check_error(ret, "Error in MPI_Recv, abs_id " + abs_id);
-
+                MPI_Recv(static_cast<void *>(rec_buf), count, MPI_CHAR, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
                 std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " received work " << rec_buf << " from " << status.MPI_SOURCE << std::endl;
                 MPI_Send(0, 0, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD);
                 std::cout << std::string(mpi_id_, '\t') << "SLAVE " << mpi_id_ << " sent answer to node " << status.MPI_SOURCE << " tag " << status.MPI_TAG << std::endl;
@@ -135,17 +119,13 @@ int main(int argc, char *argv[])
     // initialization
     tbb::task_scheduler_init init();
 
-    int ret;
     int mpi_rank, mpi_id, hostname_len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
 
-    ret = MPI_Init(&argc, &argv);
-    check_error(ret, "Error in MPI_Init");
+    MPI_Init(&argc, &argv);
 
-    ret = MPI_Comm_size(MPI_COMM_WORLD, &mpi_rank);
-    check_error(ret, "Error in MPI_Comm_size");
-    ret = MPI_Comm_rank(MPI_COMM_WORLD, &mpi_id);
-    check_error(ret, "Error in MPI_Comm_rank");
+    MPI_Comm_size(MPI_COMM_WORLD, &mpi_rank);
+    MPI_Comm_rank(MPI_COMM_WORLD, &mpi_id);
     MPI_Get_processor_name(hostname, &hostname_len);
 
     MPI_MASTER_THREAD = mpi_rank - 1;
@@ -181,7 +161,6 @@ int main(int argc, char *argv[])
         tbb::parallel_for_each(receivers.begin(), receivers.end(), [&](Receiver &r) { r(); } );
     }
 
-    ret = MPI_Finalize();
-    check_error(ret, "Error in MPI_Finalize");
+    MPI_Finalize();
 }
 
